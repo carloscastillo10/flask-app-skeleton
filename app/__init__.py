86ad58a3f88058fae7ebd1c1bd8f4e24603933f9
carloscastillo10 <carloscastillo090916@gmail.com>
2022-03-04 09:54:30 -0500

    :hammer: Updating project initialization

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,14 @@ from importlib import import_module
 
 
 def register_blueprints(app):
-    modules = ['home']
+    modules = ['base']
     for module_name in modules:
         module = import_module('app.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
 
 def create_app(config):
-    app = Flask(__name__, static_folder='static')
+    app = Flask(__name__, static_folder='base/static')
     app.config.from_object(config)
     register_blueprints(app)
     return app
