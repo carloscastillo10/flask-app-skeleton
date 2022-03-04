fe6316ded52b9d41a99736d769c4dcae50967053
carloscastillo10 <carloscastillo090916@gmail.com>
2022-03-04 09:49:22 -0500

    :sparkles: Initialize project

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from importlib import import_module
+
+
+def register_blueprints(app):
+    modules = ['home']
+    for module_name in modules:
+        module = import_module('app.{}.routes'.format(module_name))
+        app.register_blueprint(module.blueprint)
+
+
+def create_app(config):
+    app = Flask(__name__, static_folder='static')
+    app.config.from_object(config)
+    register_blueprints(app)
+    return app
