df65768a41a312c517578358e1713833a0da1af9
carloscastillo10 <carloscastillo090916@gmail.com>
2022-03-04 11:38:21 -0500

    :hammer: Updating routes

--- app/base/routes.py
+++ app/base/routes.py
@@ -6,18 +6,3 @@ from flask import render_template
 @blueprint.route('/index')
 def index():
     return render_template('index.html')
-
-
-@blueprint.errorhandler(403)
-def access_forbidden(error):
-    return render_template('page-403.html'), 403
-
-
-@blueprint.errorhandler(404)
-def not_found_error(error):
-    return render_template('page-404.html'), 404
-
-
-@blueprint.errorhandler(500)
-def internal_error(error):
-    return render_template('page-500.html'), 500
